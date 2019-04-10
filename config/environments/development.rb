8db22d38588fedca1dff2bf5d0b12f6e74cc712e
Kenzie Ryan <kenzkenz314@gmail.com>
2019-04-10 00:54:58 +0000

    styleing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,17 @@
 Rails.application.configure do
+
+config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
+  
   config.action_mailer.default_url_options = { host: 'nomster-kenzie-ryan.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -60,3 +73,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+config.action_mailer.delivery_method = :test
